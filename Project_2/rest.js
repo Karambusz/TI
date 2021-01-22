9c801f82dbe8ae684211d3d8fa892b9cb47d790d
Vadym <vsamkovich@ukr.net>
2021-01-22 19:58:49 +0100

    added a questionnaire validation

--- Project_2/rest.js
+++ Project_2/rest.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", ()=> {
     
-    if(checkCookie() == true) {
+    if(checkCookie().length > 0) {
         document.querySelector(".menu-wrapper").innerHTML = 
         `
         <div><button class="btn btn-questionnaire">Ankieta</button></div>
@@ -43,96 +43,165 @@ window.onload = function() {
             console.log("click register");
         }      
     });
-    }
+
+    document.querySelector(".main-content").addEventListener("click", (e)=> {
+        if(e.target && e.target.classList.contains("btn-send")) {
+            let check = validateRadioButtons();
+            if(check && navigator.onLine == true && checkCookie().length > 0) {
+                console.log("Online send");
+            }
+            if(check && navigator.onLine == false) {
+                console.log("Offline send");
+            }
+            if(check && checkCookie()== "") {
+                console.log("Offline send");
+            }    
+        }      
+    });
+}
 
 
 function checkCookie() {
     if(document.cookie === undefined) {
-        return false;
+        return "";
     }
     if(document.cookie.includes("sessionID") === false) {
-        return false;
+        return "";
     }
     let arr = document.cookie.split(";");
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].includes("sessionID")) {
             let session = arr[i].split("=");
                if(session[1].length > 0) {
-                    return true;
+                    return session[1];
                 } else {
-                    return false;
+                    return "";
                 }
         }
     }
 }
+
+
 
 function validateEmail(email) {
         var re = /\S+@\S+\.\S+/;
         return re.test(email);
 }
 
+function validateRadioGroup(arr, pred) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].checked) {
+            pred = true;
+        }    
+    }    
+    return pred;
+}
+
+function uncheckedButtons(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].checked = false;
+    }
+}
+
 function getQuestionnaire() {
     return `
     <div class="questionnaire">
-    <div class="questionnaire-wrapper">
-        <p>1. Przoszę podać płeć</p>
-        <div class="wrapper-item">
-            <input type="radio" id="woman" name="sex" value="kobieta">
-            <label for="woman">Kobieta</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="man" name="sex" value="mężczyzna">
-            <label for="man">Mężczyzna</label>
-        </div>
+        <form method="POST">
+            <div class="questionnaire-wrapper">
+                <p>1. Przoszę podać płeć</p>
+                <div class="wrapper-item">
+                    <input type="radio" id="woman" name="gender" value="kobieta">
+                    <label for="woman">Kobieta</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="man" name="gender" value="mężczyzna">
+                    <label for="man">Mężczyzna</label>
+                </div>
 
-        <p>2. Jaki tytuł Pan/i uzyskał/a?</p>
-        <div class="wrapper-item">
-            <input type="radio" id="licentiate" name="degree" value="lic">
-            <label for="licentiate">Licencjat</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="engineer" name="degree" value="inż">
-            <label for="engineer">Inżynier</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="master" name="degree" value="mgr">
-            <label for="master">Magister</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="doctor" name="degree" value="dr">
-            <label for="doctor">Doktor</label>
-        </div>
+                <p>2. Jaki tytuł Pan/i uzyskał/a?</p>
+                <div class="wrapper-item">
+                    <input type="radio" id="licentiate" name="degree" value="lic">
+                    <label for="licentiate">Licencjat</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="engineer" name="degree" value="inż">
+                    <label for="engineer">Inżynier</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="master" name="degree" value="mgr">
+                    <label for="master">Magister</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="doctor" name="degree" value="dr">
+                    <label for="doctor">Doktor</label>
+                </div>
 
-        <p>3. Czy uczelnia jest pomocna w rozwoju Waszej kariery zawodowej?</p>
-        <div class="wrapper-item">
-            <input type="radio" id="help_yes" name="help" value="tak">
-            <label for="help_yes">Tak</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="help_no" name="help" value="nie">
-            <label for="engineer">Nie</label>
-        </div>
+                <p>3. Czy uczelnia jest pomocna w rozwoju Waszej kariery zawodowej?</p>
+                <div class="wrapper-item">
+                    <input type="radio" id="help_yes" name="help" value="tak">
+                    <label for="help_yes">Tak</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="help_no" name="help" value="nie">
+                    <label for="engineer">Nie</label>
+                </div>
 
-        <p>4. Proszę określić stopień zadowolenia ze studiów wyższych</p>
-        <div class="wrapper-item">
-            <input type="radio" id="vhappy" name="satisfaction" value="Bardzo zadowolony/a">
-            <label for="vhappy">Bardzo zadowolony/a</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="happy" name="satisfaction" value="Zadowolony/a">
-            <label for="happy">Zadowolony/a</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="dissatisfied" name="satisfaction" value="Niezadowolony/a">
-            <label for="dissatisfied">Niezadowolony/a</label>
-        </div>
-        <div class="wrapper-item">
-            <input type="radio" id="vdissatisfied" name="satisfaction" value="Bardzo niezadowolony/a">
-            <label for="vdissatisfied">Bardzo niezadowolony/a</label>
-        </div>
-    </div>
-</div> 
+                <p>4. Proszę określić stopień zadowolenia ze studiów wyższych</p>
+                <div class="wrapper-item">
+                    <input type="radio" id="vhappy" name="satisfaction" value="Bardzo zadowolony/a">
+                    <label for="vhappy">Bardzo zadowolony/a</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="happy" name="satisfaction" value="Zadowolony/a">
+                    <label for="happy">Zadowolony/a</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="dissatisfied" name="satisfaction" value="Niezadowolony/a">
+                    <label for="dissatisfied">Niezadowolony/a</label>
+                </div>
+                <div class="wrapper-item">
+                    <input type="radio" id="vdissatisfied" name="satisfaction" value="Bardzo niezadowolony/a">
+                    <label for="vdissatisfied">Bardzo niezadowolony/a</label>
+                </div>
+                <div class="wrapper-item">
+                    <input class="btn btn-send" type='button' value='Wyślij' style="display: block; margin:0 auto;">
+                </div> 
+            </div>
+            <p class="error"></p>     
+        </form> 
+    </div> 
     `;
+}
+
+function validateRadioButtons() {
+    const genders = document.querySelectorAll("[name = 'gender']"), 
+            academicTitles =  document.querySelectorAll("[name = 'degree']"),
+            helpful = document.querySelectorAll("[name = 'help']"),
+            satisfaction = document.querySelectorAll("[name = 'satisfaction']");
+
+            console.log(genders);
+
+    let gen = false,
+        ac = false;
+        help = false;
+        sat = false;
+
+        gen = validateRadioGroup(genders, gen);
+        ac = validateRadioGroup(academicTitles, ac);
+        help = validateRadioGroup(helpful, help);
+        sat = validateRadioGroup(satisfaction, sat);
+
+    if (gen && ac && help && sat) {
+        document.querySelector("p.error").innerHTML="All checked";
+        uncheckedButtons(genders);
+        uncheckedButtons(academicTitles);
+        uncheckedButtons(helpful);
+        uncheckedButtons(satisfaction);
+        return true; 
+    } else {
+        document.querySelector("p.error").innerHTML="Proszę odpowiedzieć na wszystkie pytania!";
+        return false;
+    }
 }
 
 function getLogin() {
